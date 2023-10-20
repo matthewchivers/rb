309e1e388fa73e8121e9835900b771bc70fdb887
Matthew Chivers <matthewchivers@outlook.com>
2023-10-20 22:15:13 +0100

    added .gitattributes

--- main.go
+++ main.go
@@ -1,9 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"github.com/matthewchivers/rb/cmd"
+)
 
 func main() {
-	fmt.Println("Welcome to Repo Butler (rb), your repository concierge.")
-	fmt.Println("I'll streamline your workflow by cloning repositories into predefined directories.")
-	fmt.Println("For instance, Go repos can go straight to your GOPATH, and I can keep work and hobby projects distinct.")
+	cmd.Execute()
 }
