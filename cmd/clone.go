2e3d801c0a410f52405c9c010d78a90986ddd6d9
Matthew Chivers <matthewchivers@outlook.com>
2023-10-22 14:38:00 +0100

    added more comprehensive README
    added basic git clone functionality

--- cmd/clone.go
+++ cmd/clone.go
@@ -3,6 +3,7 @@ package cmd
 import (
 	"fmt"
 
+	"github.com/matthewchivers/rb/gitutils"
 	"github.com/spf13/cobra"
 )
 
@@ -13,25 +14,29 @@ var (
 
 var cloneCmd = &cobra.Command{
 	Use:   "clone [repo-url]",
-	Short: "clone a repository into a directory",
-	Long:  "clone a repository into a directory based on the user-specified ruleset",
+	Short: "Clone a repository into a directory",
+	Long:  "Clone a repository into a directory based on the user-specified ruleset",
 	Args:  cobra.ExactArgs(1),
 	Run:   CloneHandler,
 }
 
 func init() {
-	cloneCmd.Flags().StringVarP(&customPath, "path", "p", "", "custom path to clone repository into")
-	cloneCmd.Flags().StringVarP(&rule, "rule", "r", "", "rule to apply when cloning repository")
+	cloneCmd.Flags().StringVarP(&customPath, "path", "p", "", "Custom path to clone repository into")
+	cloneCmd.Flags().StringVarP(&rule, "rule", "r", "", "Rule to apply when cloning repository")
 	RootCmd.AddCommand(cloneCmd)
 }
 
 func CloneHandler(cmd *cobra.Command, args []string) {
 	repositoryURL := args[0]
-	if customPath != "" {
-		fmt.Printf("Cloning repository: %s into %s\n", repositoryURL, customPath)
-	} else if rule != "" {
-		fmt.Printf("Cloning repository: %s using rule: %s\n", repositoryURL, rule)
-	} else {
-		fmt.Printf("Cloning repository: %s\n", repositoryURL)
+	if customPath == "" {
+		fmt.Errorf("Error: No target path specified\n")
+		return
+	}
+	cloneTargetPath := customPath // Simplify by directly using customPath
+	if rule != "" {
+		fmt.Printf("TODO: Implement rule: %s\n", rule)
+	}
+	if err := gitutils.CloneGitRepo(repositoryURL, cloneTargetPath); err != nil { // Simplified error handling
+		fmt.Println("Error cloning repository:", err)
 	}
 }
