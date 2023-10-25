60893c3090473d6f9f852ec072b735b9236775bd
Matthew Chivers <matthewchivers@outlook.com>
2023-10-25 15:11:41 +0000

    Added case to handle when no rule matches

--- pkg/config/clone_rules.go
+++ pkg/config/clone_rules.go
@@ -1,12 +1,16 @@
 package config
 
 import (
+	"errors"
 	"regexp"
 
 	"github.com/matthewchivers/rb/pkg/fsutil"
 	pr "github.com/matthewchivers/rb/pkg/pathresolver"
 )
 
+// GetDirectory returns the directory to clone the repository into
+// based on the rules in the config file.
+// If no rules match, an error is returned.
 func (c *Config) GetDirectory(repoOwner, repoName string) (string, error) {
 	defaultRule := Rule{}
 	for _, rule := range c.Rules {
@@ -25,6 +29,9 @@ func (c *Config) GetDirectory(repoOwner, repoName string) (string, error) {
 		if rule.Default {
 			defaultRule = rule
 		}
+	}
+	if defaultRule.Directory == "" {
+		return "", errors.New("no matching rule found")
 	}
 	return pr.ExpandPath(fsutil.OSFileSystem{}, defaultRule.Directory)
 }
