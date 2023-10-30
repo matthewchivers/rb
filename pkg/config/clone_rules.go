31c50e9dad1384d9a479f9bca1148c79bed2cbbe
Matthew Chivers <matthewchivers@outlook.com>
2023-10-30 15:42:32 +0000

    overhauled configuration file and rules as well as adding logging for verbose output / debug

--- pkg/config/clone_rules.go
+++ pkg/config/clone_rules.go
@@ -24,53 +24,63 @@ func (e *NoRuleError) Error() string {
 // based on the rules in the config file.
 // If no rules match, an error is returned.
 func (c *Config) GetDirectory(repo *types.Repo) (string, error) {
-	defaultRule := Rule{}
-	for _, rule := range c.Rules {
-		allMatched := checkRepoMatches(rule, repo)
-		if allMatched {
-			return getRulePath(rule, repo)
+	logger.Infof("Getting directory for repository %s", repo.Name)
+	var defaultRule *CloneRule
+	for _, rule := range c.CloneRules {
+		logger.Debugf("Checking rule %s", rule.Name)
+		if checkRepoMatches(rule, repo) {
+			logger.Debugf("Rule %s matched the repository", rule.Name)
+			return c.getRulePath(rule, repo)
 		}
 		if rule.Default {
-			defaultRule = rule
+			logger.Debugf("Rule %s is default", rule.Name)
+			defaultRule = &rule
 		}
 	}
-	if defaultRule.Directory == "" {
-		return "", &NoRuleError{
-			Msg:  "No rule found for repository",
-			Code: 1,
-		}
+	if defaultRule != nil {
+		logger.Debugf("No rule matched the repository, using default rule %s", defaultRule.Name)
+		return c.getRulePath(*defaultRule, repo)
 	}
-	return pathparser.ParsePath(fsutil.OSFileSystem{}, defaultRule.Directory)
+	logger.Debugf("No rule matched the repository")
+	return "", &NoRuleError{Msg: "No rule matched the repository", Code: 1}
 }
 
-// getRulePath returns the path to clone the repository into based on the supplied rule
-func getRulePath(rule Rule, repo *types.Repo) (string, error) {
-	rulePath, err := pathparser.ParsePath(fsutil.OSFileSystem{}, rule.Directory)
-	if err != nil {
-		return "", err
+// getRulePath returns the path to clone the repository into based on the rule.
+func (c *Config) getRulePath(rule CloneRule, repo *types.Repo) (string, error) {
+	dirMap := make(map[string]string)
+	for _, directory := range c.Directories {
+		dirMap[directory.Name] = directory.Path
 	}
-
-	var clonePath string
-	if rule.Nesting.Pattern != "" {
-		nestedPath, err := getNestedPath(rule.Nesting.Pattern, repo)
+	logger.Debugf("Directories: %v", dirMap)
+	if _, ok := dirMap[rule.DirectoryName]; !ok {
+		return "", fmt.Errorf("directory name %s not found in config", rule.DirectoryName)
+	}
+	logger.Infof("Directory name %s found in rule %s", rule.DirectoryName, rule.Name)
+	path := dirMap[rule.DirectoryName]
+	logger.Debugf("Base path: %s", path)
+	if rule.NestingPattern != "" {
+		nestedPath, err := getNestedPath(rule.NestingPattern, repo)
+		logger.Debugf("Nested path: %s", nestedPath)
 		if err != nil {
 			return "", err
 		}
-		clonePath = rulePath + nestedPath
-	} else {
-		clonePath = rulePath
+		// return dirMap[rule.DirectoryName] + nestedPath, nil
+		path = path + nestedPath
 	}
-
-	return clonePath + "/" + repo.Name, nil
+	repoPath := path + "/" + repo.Name
+	logger.Debugf("Rule path: %s", repoPath)
+	return repoPath, nil
 }
 
 // getNestedPath processes the nesting pattern and returns the nested path.
 func getNestedPath(pattern string, repo *types.Repo) (string, error) {
 	// Validate the pattern
+	logger.Debugf("Validating pattern: %s", pattern)
 	validPattern := regexp.MustCompile(`^([\w/]*\{(?:host|owner)\}[\w/]*)+$`)
 	if !validPattern.MatchString(pattern) {
 		return "", fmt.Errorf("invalid pattern: %s", pattern)
 	}
+	logger.Debugf("Pattern %s is valid", pattern)
 	// Perform replacements
 	nestedPath := strings.ReplaceAll(pattern, "{host}", repo.Host)
 	nestedPath = strings.ReplaceAll(nestedPath, "{owner}", repo.Owner)
@@ -78,17 +88,24 @@ func getNestedPath(pattern string, repo *types.Repo) (string, error) {
 	if err != nil {
 		return "", err
 	}
+	logger.Infof("Nested path: %s", nestedPath)
 	return nestedPath, nil
 }
 
-func checkRepoMatches(rule Rule, repo *types.Repo) bool {
+// checkRepoMatches checks if the repository matches the rule.
+func checkRepoMatches(rule CloneRule, repo *types.Repo) bool {
 	allMatched := true
+	logger.Debugf("Checking if repository %s matches rule %s", repo.Name, rule.Name)
 	if rule.Match.Repository.Owner != "" {
 		matched, _ := regexp.MatchString(rule.Match.Repository.Owner, repo.Owner)
 		allMatched = allMatched && matched
 	}
 	if rule.Match.Repository.Name != "" {
 		matched, _ := regexp.MatchString(rule.Match.Repository.Name, repo.Name)
+		allMatched = allMatched && matched
+	}
+	if rule.Match.Repository.Host != "" {
+		matched, _ := regexp.MatchString(rule.Match.Repository.Host, repo.Host)
 		allMatched = allMatched && matched
 	}
 	return allMatched
--- pkg/config/config.go
+++ pkg/config/config.go
@@ -4,62 +4,82 @@ import (
 	"os"
 
 	"github.com/matthewchivers/rb/pkg/fsutil"
+	rblogger "github.com/matthewchivers/rb/pkg/logger"
 	"github.com/matthewchivers/rb/pkg/pathparser"
 	"gopkg.in/yaml.v2"
 )
 
-// Config is the struct representation of the config file
+var (
+	logger = rblogger.GetLogger()
+)
+
+// Directory represents a directory in the config file
+type Directory struct {
+	Name string `yaml:"name"`
+	Path string `yaml:"path"`
+}
+
+// Repository represents a repository in the config file
+type Repository struct {
+	URL           string `yaml:"url"`
+	DirectoryName string `yaml:"directory_name"`
+	RelativePath  string `yaml:"relative_path"`
+}
+
+// MatchRepository represents a repository in a match rule
+type MatchRepository struct {
+	Name  string `yaml:"name"`
+	Owner string `yaml:"owner"`
+	Host  string `yaml:"host"`
+}
+
+// CloneRule represents a clone rule in the config file
+type CloneRule struct {
+	Name           string `yaml:"name"`
+	Default        bool   `yaml:"default"`
+	DirectoryName  string `yaml:"directory_name"`
+	NestingPattern string `yaml:"nesting_pattern"`
+	Match          struct {
+		// TODO: Add support for multiple matches
+		Repository MatchRepository `yaml:"repository"`
+	} `yaml:"match"`
+}
+
+// Config represents the config file
 type Config struct {
-	Rules []Rule `yaml:"rules"`
-}
-
-// Rule is the struct representation of a rule in the config file
-type Rule struct {
-	Name      string    `yaml:"name"`
-	Directory string    `yaml:"directory"`
-	Nesting   Nesting   `yaml:"nesting,omitempty"`
-	Default   bool      `yaml:"default"`
-	Match     MatchRule `yaml:"match,omitempty"`
-}
-
-// Nesting is the struct representation of the nesting configuration in the config file
-type Nesting struct {
-	Pattern string `yaml:"pattern,omitempty"`
-}
-
-// MatchRule is the struct representation of the match rule in the config file
-type MatchRule struct {
-	Repository RepositoryRule `yaml:"repository"`
-}
-
-// RepositoryRule is the struct representation of the repository rule in the config file
-type RepositoryRule struct {
-	Owner string `yaml:"owner,omitempty"`
-	Name  string `yaml:"name,omitempty"`
+	Directories  []Directory  `yaml:"directories"`
+	Repositories []Repository `yaml:"repositories"`
+	CloneRules   []CloneRule  `yaml:"clone_rules"`
 }
 
 // LoadConfig loads the config file from the specified path
 func LoadConfig(filePath string) (*Config, error) {
+	logger.Infof("Loading config file from %s", filePath)
 	osfs := fsutil.OSFileSystem{}
 	parsedPath, err := pathparser.ParsePath(osfs, filePath)
 	if err != nil {
+		logger.Errorf("Could not parse path: %s", err)
 		return nil, err
 	}
 
 	_, err = os.Stat(parsedPath)
 	if err != nil {
+		logger.Errorf("File does not exist: %s", err)
 		return nil, err
 	}
 
+	logger.Debugf("Reading config file %s", parsedPath)
 	data, err := os.ReadFile(parsedPath)
 	if err != nil {
 		return nil, err
 	}
+	logger.Debugf("Config file contents: %s", data)
 
 	var config Config
 	err = yaml.Unmarshal(data, &config)
 	if err != nil {
 		return nil, err
 	}
+	logger.Infof("Loaded config file from %s", filePath)
 	return &config, nil
 }
